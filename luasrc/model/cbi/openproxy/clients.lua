d9d431fbdd7e1aae5eea13608e6a348825abcf7c
parsamoh <parsamohammadnezhad@gmail.com>
2025-12-31 19:46:24 +0330

    feat: Add dynamic proxy group selection for clients in the UI and integrate it into configuration generation.

--- luasrc/model/cbi/openproxy/clients.lua
+++ luasrc/model/cbi/openproxy/clients.lua
@@ -20,4 +20,44 @@ o.datatype = "ipaddr"
 o.rmempty = false
 o.description = translate("Static IP address of the device")
 
+-- Simple parsing to get available proxy groups from current config
+local config_file = uci:get("openproxy", "config", "config_path")
+local fs = require "nixio.fs"
+
+o = s:option(MultiValue, "proxies", translate("Proxy Groups"))
+o.description = translate("Select which proxy groups/providers should be available for this device.")
+o.optional = false
+o.rmempty = false
+o:value("DIRECT")
+o:value("REJECT")
+o:value("⭐Default Node Group⭐") -- Default fallback
+
+if config_file and fs.access(config_file) then
+    local file = io.open(config_file, "r")
+    if file then
+        local in_groups = false
+        for line in file:lines() do
+            -- Very basic state machine to find proxy-groups section
+            if line:match("^proxy%-groups:") then
+                in_groups = true
+            elseif line:match("^%S") and not line:match("^%s*#") then
+                -- Exit if we hit another top-level section (no indentation)
+                in_groups = false
+            end
+
+            if in_groups then
+                local name = line:match("^%s*-%s*name:%s*(.+)")
+                if name then
+                    -- Trim whitespace/quotes
+                    name = name:gsub("^%s*(.-)%s*$", "%1")
+                    name = name:gsub("^['\"](.-)['\"]$", "%1")
+                    o:value(name)
+                end
+            end
+        end
+        file:close()
+    end
+end
+o.default = "⭐Default Node Group⭐ DIRECT"
+
 return m
